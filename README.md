a6aa75bf49b2b349e615af15ecb4b0d5c920653e
whrfriends <whrfriends@163.com>
2023-11-20 17:20:21 +0800

    update settings.py

--- README.md
+++ README.md
@@ -2,7 +2,4 @@
 geodjango + leaflet 
 
 ### 0.0.1 version
-
-
-### 0.0.1 version
     1、add requrements.
--- geoapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeoappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geoapp'
